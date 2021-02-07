d8c8d2858ea6d194ff863c65ceb422c6baa2275c
mechanicworld <47930174+mechanicworld@users.noreply.github.com>
2021-02-07 13:09:51 +0300

    test

--- test.js
+++ test.js
@@ -1,9 +1,18 @@
 const Database = require('./database')
 const Meetup = require('./meetup')
-const loadedFile = Database.load('meetup.json')
 const Person = require('./person')
+Database.load('meetup.json', (err, loadedFile) => {
+    if(err) {
+        console.log('Hey an error occcured', err)
+        return
+    }
+    
+    const wtmb = Meetup.create(loadedFile)
+    const omur = new Person('Omur', 30)
+    omur.attend(wtmb)
+    wtmb.printAttendeeNames()
+})
 
-const wtmb = new Meetup(loadedFile.name, loadedFile.attendees)
-const omur = new Person('Omur', 30)
-omur.attend(wtmb)
-wtmb.printAttendeeNames()
+console.log("I am the last one ?")
+
+
